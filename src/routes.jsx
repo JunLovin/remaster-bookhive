f7ca289ade6222edca8598970c6f34a6d0310c47
Mathias Rendon <mathiassaid7@gmail.com>
2025-02-15 09:01:51 -0500

    Add children and dinamic url

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,19 @@
 import App from "./App";
+import Searching from '@components/Searching'
 import ErrorDefault from "./error/errorDefault";
 
 const routes = [
     {
         path: '',
         element: <App/>,
-        errorElement: <ErrorDefault/>
+        errorElement: <ErrorDefault/>,
+        children: [
+            {
+                // INFO: Defino una ruta dinámica para que cambie cada vez que el usuario busca un libro.
+                path: '/:book',
+                element: <Searching/>,
+            }
+        ]
     }
 ]
 
