c6d2280e1e80f3ba7a88394bfadd0d8e9678b293
Mathias Rendon <mathiassaid7@gmail.com>
2025-02-16 10:50:20 -0500

    Change window.open to useNavigate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,18 @@
+import { useNavigate } from 'react-router-dom';
 import '@styles/App.css'
 
 function Header() {
+    const navigate = useNavigate()
 
     const handleHome = () => {
-        window.open('/', '_self');
+        navigate('/')
     }
 
     return (
         <>
         <header>
             <div className="header-left">
-                <h1>BookHive</h1>
+                <h1 onClick={handleHome}>BookHive</h1>
             </div>
             <div className="header-right">
             <svg className="header-heart" xmlns="http://www.w3.org/2000/svg"  width="32"  height="32"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round" ><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" /></svg>
