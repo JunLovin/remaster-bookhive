64c5cdb7b36cd26a4ae094da9f61f5b225279251
Mathias Rendon <mathiassaid7@gmail.com>
2025-02-20 10:58:00 -0500

    Feat: add new heart to small devices inside book thumbnail

--- src/components/IndividualBook.jsx
+++ src/components/IndividualBook.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import '@styles/App.css'
+import { useNavigate } from 'react-router-dom'
 
 function IndividualBook() {
     const [bookInfo, setBookInfo] = useState({})
@@ -26,8 +27,10 @@ function IndividualBook() {
     }, [bookId])
 
     const handleFillHeart = () => {
-        const heart = document.getElementById('heart')
-        heart.classList.toggle('fill')
+        const heartpc = document.getElementById('heart')
+        const heartmobile = document.getElementById('heart-mobile')
+        heartpc.classList.toggle('fill')
+        heartmobile.classList.toggle('fill')
     }
 
     if (isLoading) {
@@ -37,23 +40,26 @@ function IndividualBook() {
     return (
         <>
         <div className="individualBook-container">
+        <div className="bubble-background third-bubble"></div>
             <div className="individualBook">
                 <div className="heart-container">
                     <svg className="heart" id="heart" onClick={handleFillHeart} xmlns="http://www.w3.org/2000/svg"  width="48"  height="48"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" /></svg>
                 </div>
                 <div className="individualBook-image-btns">
-                    <div className="book-image">
-                        <img 
-                            src={bookInfo.volumeInfo?.imageLinks?.large || bookInfo.volumeInfo?.imageLinks?.thumbnail || "Imagen no disponible"} 
-                            alt={bookInfo.volumeInfo?.title || "No disponible"}
-                        />
-                    </div>
+                <h2 className="mobile-title">{bookInfo.volumeInfo?.title || "Título no disponible"}</h2>
+                <div className="book-image">
+                    <img 
+                        src={bookInfo.volumeInfo?.imageLinks?.large || bookInfo.volumeInfo?.imageLinks?.thumbnail || "Imagen no disponible"} 
+                        alt={bookInfo.volumeInfo?.title || "No disponible"}
+                    />
+                    <svg className="heart-mobile" id="heart-mobile" onClick={handleFillHeart} xmlns="http://www.w3.org/2000/svg"  width="48"  height="48"  viewBox="0 0 24 24"  fill="none"  stroke="#E3E63A"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" /></svg>
+                </div>
                 </div>
                 <div className="individualBook-text">
-                    <h2>{bookInfo.volumeInfo?.title || "Título no disponible"}</h2>
+                    <h2 className="pc-title">{bookInfo.volumeInfo?.title || "Título no disponible"}</h2>
                     <h3>{bookInfo.volumeInfo?.authors.join(', ')}</h3>
                     <h4>Rating: {bookInfo.volumeInfo?.averageRating || "?"}/5</h4>
-                    <p>{bookInfo.volumeInfo?.description.substring(0, 820) + "..." || "Descripción no disponible"}</p>
+                    <p>{bookInfo.volumeInfo?.description && bookInfo.volumeInfo?.description.substring(0, 820) + "..." || "Descripción no disponible"}</p>
                     <div className="book-buttons">
                         <button onClick={() => {
                             window.open(bookInfo.volumeInfo?.previewLink, "_blank")
