d573dce599178e80e3d3809a07ad6a214a819a29
Mathias Rendon <mathiassaid7@gmail.com>
2025-02-25 10:51:17 -0500

    Delete modals and import it from modals folder, also add function props to make it work

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,8 @@
 import { useState } from 'react';
-import Modal from 'react-modal';
 import { useNavigate } from 'react-router-dom';
 import '@styles/App.css'
-
-Modal.setAppElement('#root')
+import Cart from '@modals/Cart'
+import Liked from '@modals/Liked'
 
 function Header() {
     const [isOpenCart, setIsOpenCart] = useState(false)
@@ -49,89 +48,10 @@ function Header() {
             </div>
         </header>
         {/* INFO: Liked Books */}
-        <Modal 
-                isOpen={isOpenLiked} 
-                onRequestClose={handleCloseLiked}
-                style={{
-                    overlay: {
-                        backgroundColor: 'rgba(0, 0, 0, 0.3)',
-                        backdropFilter: 'blur(4px)'
-                    },
-                    content: {
-                        display: 'flex',
-                        flexDirection: 'column',
-                        gap: '3rem',
-                        backgroundColor: '#06021A',
-                        color: '#F3F1FF',
-                        position: 'fixed',
-                        left: 'auto',
-                        right: '0',
-                        top: '0',
-                        bottom: '0',
-                        width: '400px',
-                        height: '100%',
-                        padding: '20px',
-                        borderRadius: '0',
-                        transform: 'none',
-                        margin: '0',
-                        border: 'none',
-                        overflowX: 'scroll'
-                    }
-                }}
-            >
-                <div className="liked-top">
-                    <h2 className="liked-books">Libros que te gustaron</h2>
-                    <svg className="modal-exit liked-exit" onClick={handleCloseLiked} xmlns="http://www.w3.org/2000/svg"  width="32"  height="32"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M18 6l-12 12" /><path d="M6 6l12 12" /></svg>
-                </div>
-                <div className="books-liked-container">
-                    <h3>!Lista de libros que te gustaron muy pronto!</h3>
-                </div>
-            </Modal>
+        <Liked isOpenLiked={isOpenLiked} handleCloseLiked={handleCloseLiked} />
         
         {/* INFO: Cart */}
-        <Modal 
-                isOpen={isOpenCart} 
-                onRequestClose={handleCloseCart}
-                style={{
-                    overlay: {
-                        backgroundColor: 'rgba(0, 0, 0, 0.3)',
-                        backdropFilter: 'blur(4px)'
-                    },
-                    content: {
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'space-between',
-                        backgroundColor: '#06021A',
-                        color: '#F3F1FF',
-                        position: 'fixed',
-                        left: 'auto',
-                        right: '0',
-                        top: '0',
-                        bottom: '0',
-                        width: '400px',
-                        height: '100%',
-                        padding: '20px',
-                        borderRadius: '0',
-                        transform: 'none',
-                        margin: '0',
-                        border: 'none',
-                        overflowX: 'scroll'
-                    }
-                }}
-            >
-                <div className="buy-top">
-                    <h2 className="tobuy-books">Carrito</h2>
-                    <svg className="modal-exit" onClick={handleCloseCart} xmlns="http://www.w3.org/2000/svg"  width="32"  height="32"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M18 6l-12 12" /><path d="M6 6l12 12" /></svg>
-                </div>
-                <div className="books-tobuy-container">
-                    <h3>¡Lista de compras muy pronto!</h3>
-                </div>
-                <div className="buy-modal-btn">
-                    <button onClick={() => {
-                    alert(`¡Felicidades, has comprado libro(s)!`)
-                }}>Comprar</button>
-                </div>
-            </Modal>
+        <Cart isOpenCart={isOpenCart} handleCloseCart={handleCloseCart}/>
         </>
     )
 }
