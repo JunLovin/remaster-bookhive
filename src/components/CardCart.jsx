334ff0f058d93429d6d9e067e57fc44772251653
Mathias Rendon <mathiassaid7@gmail.com>
2025-02-26 11:36:05 -0500

    Create component to each card in cart modal

---
+++ src/components/CardCart.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import '@styles/App.css'
+
+function CardCart({ thumbnail, title, deleteCard}) {
+    const [counter, setCounter] = useState(1);
+
+    const addCounter = () => {
+        setCounter(prev => prev + 1)
+    }
+
+    const minusCounter = () => {
+        if (counter > 1) {
+            setCounter(prev => prev - 1)
+        } else {
+            return
+        }
+    }
+
+    return(
+        <>
+            <div className="card-cart-container">
+                <div className="card-cart-left">
+                    <img src={thumbnail && thumbnail} alt={title && title}/>
+                </div>
+                <div className="card-cart-right">
+                    <h2>{title && title.substring(0, 35) + "..." || "No encontrado"}</h2>
+                    <div className="card-cart-increase-decrease">
+                        <button className="card-cart-increase" onClick={addCounter}>+</button>
+                        <span>{counter}</span>
+                        <button className="card-cart-decrease" onClick={minusCounter}>-</button>
+                    </div>
+                </div>
+                <span className="delete-card" onClick={deleteCard}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="#E3E63A" d="M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6zM8 9h8v10H8zm7.5-5l-1-1h-5l-1 1H5v2h14V4z"/></svg>
+                </span>
+            </div>
+        </>
+    )
+}
+
+export default CardCart
