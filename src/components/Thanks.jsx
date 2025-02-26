69e15337f7dd76c76ffa923f3c408957ebcc7621
Mathias Rendon <mathiassaid7@gmail.com>
2025-02-26 11:37:32 -0500

    Create thanks section to thank all the people who gave us feedback.

--- src/components/Thanks.jsx
+++ src/components/Thanks.jsx
@@ -1,9 +1,24 @@
 import "@styles/App.css"
+import Header from '@components/Header'
 
 function Thanks() {
     return (
         <>
-        
+        <Header/>
+        <div className="thanks">
+            <div className="bubble-background third-bubble"></div>
+            <div className="thanks-container">
+                <div className="thanks-top">
+                    <h2>Agradecimientos</h2>
+                </div>
+                <div className="thanks-bottom">
+                    <p>Quiero agradecer a todas las personas que le dieron feedback al proyecto, gracias a todos ustedes logré mejorarlo y optimizarlo. Si tú tienes algún comentario o feedback que quisieras hacerle a la página con gusto te responderé y lo tomaré en cuenta.</p>
+                    <div className="thanks-button">
+                        <button>Feedback</button>
+                    </div>
+                </div>
+            </div>
+        </div>
         </>
     )
 }
