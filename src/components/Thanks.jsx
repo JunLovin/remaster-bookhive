8a11abbfd13f0d1e37f8e96767a5ae13776a0ace
Mathias Rendon <mathiassaid7@gmail.com>
2025-02-28 11:29:07 -0500

    feat: add mailto to send feedback

--- src/components/Thanks.jsx
+++ src/components/Thanks.jsx
@@ -12,9 +12,11 @@ function Thanks() {
                     <h2>Agradecimientos</h2>
                 </div>
                 <div className="thanks-bottom">
-                    <p>Quiero agradecer a todas las personas que le dieron feedback al proyecto, gracias a todos ustedes logré mejorarlo y optimizarlo. Si tú tienes algún comentario o feedback que quisieras hacerle a la página con gusto te responderé y lo tomaré en cuenta.</p>
+                    <p>Quiero agradecer a todas las personas que le dieron feedback al proyecto, gracias a todos ustedes logré mejorarlo y optimizarlo. Si tienes algún comentario o feedback que quisieras darle al proyecto con gusto te responderé y lo tomaré en cuenta.</p>
                     <div className="thanks-button">
-                        <button>Feedback</button>
+                        <button onClick={() => {
+                            window.open('mailto:mathiassaid7@gmail.com', '_self');
+                        }}>Feedback</button>
                     </div>
                 </div>
             </div>
